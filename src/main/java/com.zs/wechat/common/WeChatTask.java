4175bbb7f01ee2fee05ae51607122894d1e1dff6
zhangshu0 <780031741@qq.com>
2018-12-25 19:44:46 +0800

    fix

--- src/main/java/com.zs/wechat/common/WeChatTask.java
+++ src/main/java/com.zs/wechat/common/WeChatTask.java
@@ -33,20 +33,20 @@ public class WeChatTask {
         String access_token = JSONObject.fromObject(jstoken).getString(
                 "access_token"); // 获取到 token 并赋值保存
         GlobalConstants.interfaceUrlProperties.put("access_token", access_token);
-
         //获取 jsticket 的执行体
-//        params.clear();
-//        params.put("access_token", access_token);
-//        params.put("type", "jsapi");
-//        String jsticket = HttpUtils.sendGet(
-//                GlobalConstants.getInterfaceUrl("ticketUrl"), params);
-//        String jsapi_ticket = JSONObject.fromObject(jsticket).getString(
-//                "ticket");
-//        GlobalConstants.interfaceUrlProperties
-//                .put("jsapi_ticket", jsapi_ticket); // 获取到 js-SDK 的 ticket 并赋值保存
-//
-//        System.out.println("jsapi_ticket================================================" + jsapi_ticket);
-//        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"token 为=============================="+access_token);
+        //        params.clear();
+        //        params.put("access_token", access_token);
+        //        params.put("type", "jsapi");
+        //        String jsticket = HttpUtils.sendGet(
+        //                GlobalConstants.getInterfaceUrl("ticketUrl"), params);
+        //        String jsapi_ticket = JSONObject.fromObject(jsticket).getString(
+        //                "ticket");
+        //        GlobalConstants.interfaceUrlProperties
+        //                .put("jsapi_ticket", jsapi_ticket); // 获取到 js-SDK 的 ticket 并赋值保存
+        //
+        //        System.out.println("jsapi_ticket================================================" + jsapi_ticket);
+        //        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"token 为=============================="+access_token);
+
 
     }
 
